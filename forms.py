18690ca1de8fc5566c5c1131ad56c02347b11fda
apoorvsingh090 <f2016690@pilani.bits-pilani.ac.in>
2022-02-16 22:44:37 +0530

    mongo1

--- forms.py
+++ forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import *
 import wtforms
-from wtforms import *
+import pymongo
+from pymongo import MongoClient
+from wtforms import SelectMultipleField,SubmitField,MultipleFileField
 import investpy
+client = pymongo.MongoClient("mongodb+srv://apoorv:apoorv@cluster0.xzygt.mongodb.net/investpy?retryWrites=true&w=majority")
 class dropdown(FlaskForm):
-	ticker=wtforms.SelectMultipleField(label='Ticker',choices=[i for i in investpy.get_stocks(country="India").symbol.sort_values()])
+	cursor = client.investpy.list.find().distinct('tickerlist')
+
+	lst=[document['tickerlist'].split('.')[0] for document in cursor]
+	ticker=wtforms.SelectMultipleField(label='Ticker',choices=[i for i in lst])
 	#search=wtforms.StringField('')
 	submit=SubmitField("Submit")
-	upload=MultipleFileField()
+	upload=MultipleFileField(render_kw={'multiple': True})
