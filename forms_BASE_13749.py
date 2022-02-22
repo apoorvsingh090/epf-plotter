57c7d6b31be94fb58ae2af9f975f300016959a3e
ApoorvSingh <42289002+apoorvsingh090@users.noreply.github.com>
2022-02-22 11:57:10 +0530

    Delete forms_BASE_13749.py

--- forms_BASE_13749.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import *
-import wtforms
-from wtforms import *
-import investpy
-class dropdown(FlaskForm):
-	ticker=wtforms.SelectMultipleField(label='Ticker',choices=[i for i in investpy.get_stocks(country="India").symbol.sort_values()])
-	#search=wtforms.StringField('')
-	submit=SubmitField("Submit")
-	upload=MultipleFileField()
